867353ec7f5b62c5fc8a978e67754bed2bb506fd
cho881020 <kj_cho@idealidea.co.kr>
2019-12-15 11:37:57 +0900

    회원가입 화면 초안

--- app/src/main/java/com/tjeit/a20191215_02_signuppractice/SignUpActivity.kt
+++ app/src/main/java/com/tjeit/a20191215_02_signuppractice/SignUpActivity.kt
@@ -1,22 +1,25 @@
 package com.tjeit.a20191215_02_signuppractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : BaseActivity() {
+// SignUpActivity로 변경!
+class SignUpActivity : BaseActivity() {
 
+    var lastBackTimeValue = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 
     override fun setupEvents() {
 
+        lastBackTimeValue = 10L
     }
 
     override fun setValues() {
 
+        lastBackTimeValue = 20L
     }
 
 }
