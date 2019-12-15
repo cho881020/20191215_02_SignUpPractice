e2c4d06e4669d9ef79f154c382f56bb3ac4c8307
cho881020 <kj_cho@idealidea.co.kr>
2019-12-15 11:10:32 +0900

    Initial commit

---
+++ app/src/main/java/com/tjeit/a20191215_02_signuppractice/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.tjeit.a20191215_02_signuppractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
